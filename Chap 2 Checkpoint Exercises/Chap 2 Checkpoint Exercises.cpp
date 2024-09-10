4afe8de1c39411a0490bb9c58c4101894d5a1671
hparadox82 <aust.i.mo34@gmail.com>
2024-09-10 15:26:08 -0400

    changed from local to remote branch

--- Chap 2 Checkpoint Exercises/Chap 2 Checkpoint Exercises.cpp
+++ Chap 2 Checkpoint Exercises/Chap 2 Checkpoint Exercises.cpp
@@ -1,20 +1,23 @@
 // Chap 2 Checkpoint Exercises.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//Austin Mollard
+//10 Sep 2024
+//Project: "Chapter 2 Checkpoint Exercises"
+//Branch: "Checkpoint_2-14"
+
+//Objective for this checkpoint: Write a program that has the following character variables: first, middle, and last.
+//Store your initials in these variables then display them on the screen. Then replace code in main with this one.
+//Then, compile/run/verify results.
 
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    char first, middle, last;
+    first = 'A';
+    middle = 'C';
+    last = 'M';
+    cout << first;
+    cout << middle;
+    cout << last;
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
